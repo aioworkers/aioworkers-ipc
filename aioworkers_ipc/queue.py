587ecbd024dbda64b242e0c9ca35432be2ed6c26
Alexander Malev <malev@somedev.ru>
2018-06-26 01:43:33 +0300

    max_messages, max_message_size & stop queue

--- aioworkers_ipc/queue.py
+++ aioworkers_ipc/queue.py
@@ -7,10 +7,18 @@ from aioworkers.queue.base import AbstractQueue, score_queue
 
 @score_queue(default_score=0)
 class Queue(FormattedEntity, ExecutorEntity, AbstractQueue):
+    def set_context(self, context):
+        context.on_stop.append(self.stop)
+        super().set_context(context)
+
     def set_config(self, config):
         c = config.new_child(executor=1)
         super().set_config(c)
-        self._queue = posix_ipc.MessageQueue(self.config.key, posix_ipc.O_CREAT)
+        self._queue = posix_ipc.MessageQueue(
+            self.config.key, posix_ipc.O_CREAT,
+            max_messages=self.config.get_int('max_messages', default=posix_ipc.QUEUE_MESSAGES_MAX_DEFAULT),
+            max_message_size=self.config.get_int('max_message_size', default=posix_ipc.QUEUE_MESSAGE_SIZE_MAX_DEFAULT),
+        )
         self._timeout_put = self.config.get_float('timeout.put', null=True, default=None)
         self._timeout_get = self.config.get_float('timeout.get', null=True, default=None)
 
@@ -23,3 +31,9 @@ class Queue(FormattedEntity, ExecutorEntity, AbstractQueue):
         score, val = value
         val = self.encode(val)
         await self.run_in_executor(self._queue.send, val, self._timeout_put, score or 0)
+
+    async def stop(self):
+        try:
+            self._queue.close()
+        except AttributeError:
+            pass
